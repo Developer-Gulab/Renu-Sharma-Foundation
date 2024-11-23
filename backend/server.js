f7399398e75b6632246f6c13867042d3ccf71389
t rahul prabhu <167653990+T-Rahul-prabhu-38@users.noreply.github.com>
2024-11-23 15:13:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//testing
+
 // Database Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
