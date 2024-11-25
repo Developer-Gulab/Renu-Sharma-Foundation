68266797f3a92e6175d61bde951e0629efc70c05
Anujpal05 <ap091453@gmail.com>
2024-11-25 11:54:40 +0530

    Making Donate Page responsive

--- frontend/src/components/DonateForm.jsx
+++ frontend/src/components/DonateForm.jsx
@@ -1,52 +1,59 @@
-import React, { act, useState } from 'react'
+import React, { act, useRef, useState } from 'react'
 
 const DonateForm = () => {
     const [activeIndex, setActiveIndex] = useState(null);
+    const customInput = useRef(null);
+    const changeIndex = (i) => {
+        setActiveIndex(activeIndex === i ? null : i);
+        customInput.current.value = "";
+    }
 
     return (
         <div
-            className=" max-w-3xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden w-fit "
+            className=" m-auto lg:max-w-3xl bg-white shadow-lg rounded-lg overflow-hidden w-[95%] md:w-[90vw] lg:w-fit"
         >
             <div
-                className="bg-gradient-to-r bg-[#292350] p-6 text-white text-center"
+                className="bg-gradient-to-r bg-[#292350] lg:p-6  p-3 text-white text-center"
             >
-                <h1 className="text-3xl font-bold">Make a Difference</h1>
+                <h1 className="text-2xl lg:text-3xl font-bold">Make a Difference</h1>
                 <p className="mt-2 text-purple-200">Your donation helps us change lives!</p>
             </div>
 
             {/* Form  */}
-            <form className="p-6 space-y-6">
+            <form className="p-2 md:p-4 lg:p-6 space-y-6">
                 {/* Donation Amount  */}
                 <div>
                     <label htmlFor="amount" className="block text-[#281d77] font-medium"
                     >Select Donation Amount *</label>
-                    <div className="flex gap-4 mt-2">
+                    <div className="flex lg:gap-4 gap-3 mt-2">
                         <button
                             type="button"
-                            onClick={() => setActiveIndex(activeIndex === 1 ? null : 1)}
-                            className={`px-4 py-2  text-white rounded-lg transition-all duration-300 ${activeIndex === 1 ? "bg-[#352896]" : "hover:bg-[#352896] hover:text-[103%] bg-[#120b54]"}`}
+                            onClick={() => changeIndex(1)}
+                            className={`px-2 lg:px-4 py-2  text-white rounded-lg transition-all duration-300 ${activeIndex === 1 ? "bg-[#352896]" : "hover:bg-[#352896] hover:text-[103%] bg-[#120b54]"}`}
                         >
                             $10
                         </button>
                         <button
                             type="button"
-                            onClick={() => setActiveIndex(activeIndex === 2 ? null : 2)}
-                            className={`px-4 py-2 text-white rounded-lg transition-all duration-300  ${activeIndex === 2 ? "bg-[#352896]" : "hover:bg-[#352896] hover:text-[103%] bg-[#120b54]"}`}
+                            onClick={() => changeIndex(2)}
+                            className={`px-2 lg:px-4 py-2 text-white rounded-lg transition-all duration-300  ${activeIndex === 2 ? "bg-[#352896]" : "hover:bg-[#352896] hover:text-[103%] bg-[#120b54]"}`}
                         >
                             $20
                         </button>
                         <button
                             type="button"
-                            onClick={() => setActiveIndex(activeIndex === 3 ? null : 3)}
-                            className={`px-4 py-2 text-white rounded-lg transition-all duration-300  ${activeIndex === 3 ? "bg-[#352896]" : "hover:bg-[#352896] hover:text-[103%] bg-[#120b54]"}`}
+                            onClick={() => changeIndex(3)}
+                            className={`px-2 lg:px-4 py-2 text-white rounded-lg transition-all duration-300  ${activeIndex === 3 ? "bg-[#352896]" : "hover:bg-[#352896] hover:text-[103%] bg-[#120b54]"}`}
                         >
                             $50
                         </button>
                         <input
+                            ref={customInput}
+                            onChange={(e) => setActiveIndex(4)}
                             type="number"
                             id="amount"
                             placeholder="Enter Amont"
-                            className="flex-1 px-4 py-2 border border-[#281d77] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#281d77]"
+                            className="flex-1 px-2 w-full lg:w-auto lg:px-4 py-2 border border-[#281d77] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#281d77]"
                         />
                     </div>
                 </div>
@@ -60,7 +67,7 @@ const DonateForm = () => {
                         type="text"
                         id="name"
                         placeholder="Enter your name"
-                        className="w-full px-4 py-2 mt-2 border border-[#281d77] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#281d77]"
+                        className="w-full px-2 lg:px-4 py-2 mt-2 border border-[#281d77] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#281d77]"
                     />
                 </div>
 
@@ -71,7 +78,7 @@ const DonateForm = () => {
                         type="email"
                         id="email"
                         placeholder="Enter your email"
-                        className="w-full px-4 py-2 mt-2 border border-[#281d77] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#281d77]"
+                        className="w-full px-2 lg:px-4 py-2 mt-2 border border-[#281d77] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#281d77]"
                     />
                 </div>
 
@@ -81,7 +88,7 @@ const DonateForm = () => {
                     >Make it Recurring?</label>
                     <select
                         id="recurring"
-                        className="w-full px-4 py-2 mt-2 border border-[#281d77] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#281d77]"
+                        className="w-full px-2 lg:px-4 py-2 mt-2 border border-[#281d77] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#281d77]"
                     >
                         <option value="one-time">One-Time</option>
                         <option value="monthly">Monthly</option>
@@ -92,7 +99,7 @@ const DonateForm = () => {
                 {/* Submit Button */}
                 <button
                     type="submit"
-                    className="w-full bg-[#292350] text-white px-4 py-2 hover:text-[104%] font-semibold rounded-lg hover:bg-[#352896] transition duration-200"
+                    className="w-full bg-[#292350] text-white px-2 lg:px-4 py-2 hover:text-[104%] font-semibold rounded-lg hover:bg-[#352896] transition duration-200"
                 >
                     Donate Now
                 </button>
--- frontend/src/pages/DonatePage.jsx
+++ frontend/src/pages/DonatePage.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react'
-import DonateForm from '../components/DonateForm';
+import React, { useEffect, useRef } from 'react'
+import DonateForm from '../Components/DonateForm';
 import "../../public/css/donate.css"
 import { FiMail } from "react-icons/fi";
 import { IoCallOutline } from "react-icons/io5";
 import { MdOutlineLocationOn } from "react-icons/md";
+import donate1 from "/images/DonateImg/donate1.jpg"
+import donate2 from "/images/DonateImg/donate2.jpeg"
+import donate3 from "/images/DonateImg/donate3.jpg"
+import donate4 from "/images/DonateImg/donate4.jpg"
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger)
 
 const DonatePage = () => {
+    const donateImg = useRef(null);
 
     useEffect(() => {
+        const isSmallWidth = window.innerWidth <= 768;
+        const startValue = isSmallWidth ? "top 90%" : "top 85%";
+        const endValue = isSmallWidth ? "top 60%" : "top 50%";
         gsap.fromTo(".left-animated", {
             x: "-200px",
             opacity: 0
@@ -23,8 +31,8 @@ const DonatePage = () => {
                 scrollTrigger: {
                     trigger: ".left-animated",
                     scroller: "body",
-                    start: "top 70%",
-                    end: "top 50%",
+                    start: startValue,
+                    end: endValue,
                     scrub: 3,
                 }
             }
@@ -42,8 +50,8 @@ const DonatePage = () => {
                 scrollTrigger: {
                     trigger: ".right-animated",
                     scroller: "body",
-                    start: "top 70%",
-                    end: "top 50%",
+                    start: startValue,
+                    end: endValue,
                     scrub: 3,
                 }
             }
@@ -72,20 +80,38 @@ const DonatePage = () => {
     }, [])
 
 
+    useEffect(() => {
+        const imgArray = [donate1, donate2, donate3, donate4];
+        let i = 0;
+
+        //Dynamically change image using setInterval();
+        const interval = setInterval(() => {
+            if (donateImg.current) {
+                donateImg.current.src = imgArray[i];
+                i = (i + 1) % imgArray.length;
+            }
+        }, 5000);
+
+        //Cleanup on component unmount
+        return () => clearInterval(interval);
+    }, [])
+
+
+
     return (
-        <div className='bg-purple-50 min-h-screen'>
+        <div className='bg-purple-50 min-h-screen pb-44 w-screen overflow-hidden'>
             <div className='pb-4 relative'>
-                <img src="../../public/images/DonateImg/heroDonate.jpg" className=' max-h-[450px] w-screen opacity-85' alt="Donate Hero Image" />
+                <img src="/images/DonateImg/heroDonate.jpg" className=' max-h-[450px] max-w-screen w-screen opacity-85' alt="Donate Hero Image" />
                 <div className=' text-center text-4xl font-semibold py-3 absolute inset-0 flex justify-center items-center '>
-                    <p className=' text-6xl text-[#281d77]  heading text-container' >Donate Some Happiness</p>
+                    <p className=' text-3xl md:text-5xl lg:text-6xl text-[#281d77]  heading text-container' >Donate Some Happiness</p>
                 </div>
             </div>
-            <p className='  text-center px-20 text-container'>
+            <p className='  text-center lg:px-20 px-2 text-container'>
                 Join us in making a lasting impact! Your generous donations to RenuHealthcare enable us to deliver essential healthcare services, social initiatives, and
                 educational programs to underserved communities. Together, we can create a healthier, more compassionate world.
             </p>
-            <h2 className=' text-4xl font-semibold text-center pt-8 pb-2 text-container'>Show Your Support</h2>
-            <p className=' text-center pb-2 text-container'>Fill the following details to provide your support for the cause</p>
+            <h2 className=' text-3xl lg:text-4xl font-semibold text-center pt-8 pb-2 text-container'>Show Your Support</h2>
+            <p className=' text-center pb-2 text-container px-2'>Fill the following details to provide your support for the cause</p>
             <div>
                 <div className="flex flex-col lg:flex-row justify-center lg:items-center gap-3 lg:gap-8 ml-6 lg:ml-0 py-5 pb-14 text-container">
                     <div className="flex justify-start lg:justify-center items-center gap-4">
@@ -113,15 +139,14 @@ const DonatePage = () => {
                     </div>
                 </div>
             </div>
-            <div className=' flex items-center justify-center px-16 w-screen '>
-                <div className=' left-animated'>
+            <div className=' flex flex-col lg:flex-row items-center justify-center lg:px-16 px-3 w-screen gap-5'>
+                <div className=' left-animated order-2 lg:order-1 '>
                     <DonateForm />
                 </div>
-                <div className=' w-[60%]  right-animated '>
-                    <img src="../../public/images/DonateImg/donate1.jpg" alt="Donate Image" className=' mx-7 rounded-lg' />
+                <div className='w-full md:w-[90%] lg:w-[60%]  right-animated order-1 lg:order-2 overflow-hidden max-h-[80%]'>
+                    <img ref={donateImg} src={donate1} alt="Donate Image" className=' lg:mx-7 rounded-lg ' />
                 </div>
             </div>
-
         </div>
     )
 }
